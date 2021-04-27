15e82e502e4f3c1933360497197460ffe4c3ca16
Daniel Sazonov <p.294803@gmail.com>
2021-04-27 20:38:05 +0300

    Create directory for sources

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,29 @@ const gulp = require('gulp');
 const {src, dest} = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const srcFolder = 'src/';
 
-function css () {
-  return src('style.scss').pipe(sass()).pipe(dest('./css')).pipe(browserSync.stream());
+function css() {
+  return src(srcFolder + 'style.scss').pipe(sass()).pipe(dest(srcFolder + 'css')).pipe(browserSync.stream());
 }
 
-function reload () {
-  return src('./').pipe(browserSync.stream());
+function reload() {
+  return src(srcFolder).pipe(browserSync.stream());
 }
 
 gulp.task('browser_sync', function () {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: srcFolder
     },
     port: 3000,
   });
 });
 
 gulp.task('watchFiles', function () {
-  gulp.watch('style.scss', css);
-  gulp.watch('index.html', reload);
-  gulp.watch('main.js', reload);
+  gulp.watch(srcFolder + 'style.scss', css);
+  gulp.watch(srcFolder + 'index.html', reload);
+  gulp.watch(srcFolder + 'main.js', reload);
 });
 
 gulp.task('default', gulp.parallel(css, 'watchFiles', 'browser_sync'));
